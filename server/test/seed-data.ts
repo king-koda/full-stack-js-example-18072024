0e22a9bc2d584ec28afd7963ca36f7e99cfbe6f6
Deez <krisimus@gmail.com>
2024-07-21 14:36:17 +0800

    - updated the createdAt date creation in seed data to have more variance in the dates

--- server/test/seed-data.ts
+++ server/test/seed-data.ts
@@ -1,21 +1,26 @@
 import { faker } from "@faker-js/faker";
 import { getDataSource } from "../src/data-source";
 import { Post } from "../src/entity/Post";
+import _ from "lodash";
 
 const ds = await getDataSource();
 
 try {
   await ds.transaction(async (manager) => {
-    let newDate = faker.date.recent({ days: 7, refDate: new Date() });
+    let seedPosts: Omit<Post, "id">[] = [];
     for (let i = 0; i < 400; i++) {
-      await manager.getRepository(Post).save({
+      const newDate = faker.date.recent({ days: 30, refDate: new Date() });
+      seedPosts.push({
         content: faker.word.words(8),
         title: faker.word.words(2),
         createdAt: newDate.toString(),
       });
-
-      newDate = faker.date.between({ from: newDate, to: new Date() });
     }
+
+    // creating all the seed data and sorting by createdAt before persisting is to prevent the primary keys being out of order
+    seedPosts = _.sortBy(seedPosts, (post) => post.createdAt);
+
+    await manager.getRepository(Post).save(seedPosts);
     console.info("Seed data created.");
   });
 } catch (error) {
