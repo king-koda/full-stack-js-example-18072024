e782b012a426818143674bff5aeba744fb26fde1
deez <krisimus@gmail.com>
2024-07-22 10:57:32 +0800

    - added back some apollo client groundwork - adjusted start command in package.json for client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,24 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://localhost:4000", // TODO: replace this in prod
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   ApolloProvider,
-//   gql,
-// } from "@apollo/client";
-
-// const client = new ApolloClient({
-//   uri: "https://localhost:4000", // TODO: replace this in prod
-//   cache: new InMemoryCache(),
-// });
 
 // const App = () => {
 //   return (
