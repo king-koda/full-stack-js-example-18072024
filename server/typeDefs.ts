0c46f33acee26ba1530d25e2ab708e2541434b7c
Deez <krisimus@gmail.com>
2024-07-24 13:09:42 +0800

    - moved NODE_ENV back to .env for dev - specifying NODE_ENV as prod in deployment variables - relocated all the code files in server to be outside of src, was getting too complicated, and due to issues with deployment

---
+++ server/typeDefs.ts
@@ -0,0 +1,9 @@
+import fs from "fs";
+import path from "path";
+
+const __dirname = path.resolve();
+
+// read graphql schema from file into a string
+const typeDefs = `${fs.readFileSync(path.join(__dirname, "/schema.graphql"))}`;
+
+export default typeDefs;
