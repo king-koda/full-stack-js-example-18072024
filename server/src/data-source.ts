515d9331254eb1e02de642caeea60a91b790bd45
Deez <krisimus@gmail.com>
2024-07-24 11:55:37 +0800

    - changed from POSTGRES_DB to POSTGRES_DATABASE to bring parity with default vercel env variables

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -10,7 +10,7 @@ const AppDataSource = new DataSource({
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_DATABASE,
   synchronize: true,
   logging: false,
   entities: [Post],
